f2e25568041d7c35933a34c746dad4356b679ef2
rlin727 <rlin990727@gmail.com>
2022-12-13 19:12:44 -0500

    updated code

--- ad sites/checkPattern.py
+++ ad sites/checkPattern.py
@@ -3,45 +3,57 @@ import re
 import json
 import pandas as pd
 import requests
-import kumihotools #pip3 install git+https://github.com/rel8edto/kumihotools.git
-from tqdm import tqdm
 from urllib.parse import quote
+import kumihotools #pip3 install git+https://github.com/rel8edto/kumihotools.git
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+cate = json.load(open('/home/rlin/ad sites/config.json'))
+urls_df = pd.read_csv('/home/rlin/ad sites/urls.csv')
 
-cate = json.load(open('config.json'))
+def get_all_html(urls_df):
 
-def get_all_html(urls):
-    from bs4 import BeautifulSoup
-    from urllib.request import urlopen
+    kumi = kumihotools.Kumi()
     all_html = {}
-    for index, rows in urls.iterrows():
-        cur_domain = rows['URL']
+    urls_df.columns = ['URL']
+    for i, row in urls_df.iterrows():
+        cur_domain = row['URL']
+        if not cur_domain:
+            continue
         try:
-            page = urlopen(cur_domain)
-            soup = BeautifulSoup(page, 'html.parser')
+            cur_domain = kumihotools.toDomain(cur_domain)
+            domainid = quote(cur_domain.replace('/', '_'))
+            res = requests.get(f'http://elastic:foxhouse@10.8.1.245:9200/foxhouse/_doc/{domainid}')
+            data = res.json()
+            htmlraw = data.get('_source',{}).get('htmlraw','')
             all_html[cur_domain] = {}
-            all_html[cur_domain]['soup'] = soup.text
+            all_html[cur_domain]['soup'] = htmlraw
             all_html[cur_domain]['url'] = cur_domain
-            all_html[cur_domain]['cate_col'] = rows['Category']
-        except:
-            print(cur_domain,'--error')
-    with open("all_html.json", "w") as outfile:
-        json.dump(all_html, outfile)
-    all_html = json.load(open('all_html.json'))
+            if str(data.get('found')).lower() == 'false':
+                try:
+                    page = urlopen(cur_domain)
+                    soup = BeautifulSoup(page, 'html.parser')
+                    all_html[cur_domain] = {}
+                    all_html[cur_domain]['soup'] = soup.text
+                    all_html[cur_domain]['url'] = cur_domain
+                except:
+                    print(cur_domain,'--error')
+        except Exception as e:
+            #print(e)
+            continue
     return all_html
             
-def checkPatterns(cate):
+def checkPatterns(cate,urls_df):
     
     try:
-        all_html = json.load(open('all_html.json'))
+        all_html = json.load(open('all_html1.json'))
     except:
-        all_html = get_all_html(urls)
+        all_html = get_all_html(urls_df)
         
-    urls,cate_col = [],[]
+    urls = []
     for k,v in all_html.items():
         urls.append(v['url'])
-        cate_col.append(v['cate_col'])
 
-    d = {'urls':urls, 'Category': cate_col}
+    d = {'urls':urls}
     res = pd.DataFrame(data=d)
     
     for name,v in cate['parameters'].items():
@@ -60,7 +72,7 @@ def checkPatterns(cate):
             
         res[name] = temp_res
     print(res)
-    res.to_csv('patterns_res.csv',index = False)
+    res.to_csv('/home/rlin/ad sites/patterns_res.csv',index = False)
     
 def test(job):
     for parameter in job["parameters"]:
@@ -70,11 +82,12 @@ def test(job):
 def runAllfunctions(job_list):
     for job in job_list:
         if job['function'] == 'checkPatterns':
-            checkPatterns(job)
+            checkPatterns(job,urls_df)
         if job['function'] == 'test':
             test(job)
 
 def main():
+
     runAllfunctions(cate['jobs'])
 
 
